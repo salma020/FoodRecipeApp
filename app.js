ebe2d0915412b8d2cf045c6b454d05a5c43adad5
salma020 <salma.emma18@gmail.com>
2022-06-07 15:10:21 +0600

    done

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 searchBtn.addEventListener('click',getMealList);
+mealList.addEventListener('click',getMealRecipe);
+recipeCloseBtn.addEventListener('click',()=>{
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+})
 
 function getMealList(){
     let searchInputText = document.getElementById('search-input').value.trim();
@@ -11,14 +15,14 @@ function getMealList(){
     fetch(`https://themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
     .then(res =>res.json())
     .then(data =>{
-        let html =" ";
+        let html ="";
         if (data.meals) {
             data.meals.forEach(meal=>{
                 html +=`
                 
                 <div class="meal-item" data-id ="${meal.idMeal}">
                 <div class="meal-img>
-                <img src= "${meal.strMealThumb}" alt= "food" />
+                <img src= "${meal.strMealThumb}" alt= "food">
                 </div>
             <div class="meal-name">
                 <h3>${meal.strMeal}</h3>
@@ -28,13 +32,50 @@ function getMealList(){
                 
                 `;
             });
+            mealList.classList.remove('notFound');
+        }else{
+            html="sorry,we didn't find any meal !";
+            mealList.classList.add('notFound')
         }
         mealList.innerHTML = html;
     });
 }
 
+//get recipe 
 
+function getMealRecipe(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('recipe-btn')) {
+        let mealItem = e.target.parentElement.parentElement;
+       fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+       .then(res =>res.json())
+       .then(data =>mealRecipeModel(data.meals))
+    }
+
+}
+function mealRecipeModel(meal) {
+    console.log(meal)
+    meal=meal[0];
+    let html=`  
+    <h2 class="recipe-title">${meal.strMeal}</h2>
+    <p class="recipe-category">${meal.strCategory}</p>
+    <div class="recipe-instruct">
+      <h3>Instructions:</h3>
+      <p>${meal.strInstructions}</p>
+    </div>
+    <div class="recipe-meal-img">
+      <img src="${meal.strMealThumb}" alt="" />
+    </div>
+    <div class="recipe-link">
+      <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+    </div>
+    `;
+    mealDetailsContent.innerHTML=html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
     
    
     
 
+
+                 
